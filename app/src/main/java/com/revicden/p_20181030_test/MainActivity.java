81dab769de7c4b57b23da04cb978b10b581887f9
qxr1999 <928904498@qq.com>
2018-10-30 11:29:47 +0800

    乔玺润

---
+++ app/src/main/java/com/revicden/p_20181030_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.revicden.p_20181030_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
